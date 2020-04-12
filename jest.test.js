535a5ba88bb2d76fc22b9db448fefe973270c86a
lei.song <moon.song@yeahmobi.com>
2020-04-12 23:09:02 +0800

    Button组件增加测试用例

---
+++ jest.test.js
@@ -0,0 +1,18 @@
+test("test common matcher", () => {
+  expect(2 + 2).toBe(4);
+});
+
+test("test to be true or false", () => {
+  expect(1).toBeTruthy();
+  expect(0).toBeFalsy();
+});
+
+test("test number", () => {
+  expect(4).toBeGreaterThan(3);
+  expect(2).toBeLessThan(3);
+});
+
+test("test object", () => {
+  // expect({ name: "ship" }).toBe({ name: "ship" });
+  expect({ name: "ship" }).toEqual({ name: "ship" });
+});
