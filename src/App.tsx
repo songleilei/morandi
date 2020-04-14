e890f1d735815d784074b74f28d506cf27f51c06
lei.song <moon.song@yeahmobi.com>
2020-04-14 18:12:44 +0800

    对FontAwesome Icon 进行二次封装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 import Menu from "./components/Menu/menu";
 import MenuItem from "./components/Menu/menuItem";
 import SubMenu from "./components/Menu/subMenu";
+import Icon from "./components/Icon/icon";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Icon theme="danger" icon={faCoffee} />
+        <FontAwesomeIcon icon={faCoffee} size="3x" rotation={90} />
         <Menu mode="horizontal">
           <MenuItem>cool link</MenuItem>
           <MenuItem disabled>cool link 2</MenuItem>
