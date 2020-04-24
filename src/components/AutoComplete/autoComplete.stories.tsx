2aa3671055d3cc7fb8b89739012ad1b946f8206a
lei.song <moon.song@yeahmobi.com>
2020-04-25 01:07:09 +0800

    兼容不同数据类型自定义展示

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { AutoComplete } from "./autoComplete";
+import { AutoComplete, DataSourceType } from "./autoComplete";
 
 const SimpleComplete = () => {
   const lakers = [
@@ -20,8 +20,13 @@ const SimpleComplete = () => {
   ];
 
   const handleFetch = (query: string) => {
-    return lakers.filter((name) => name.includes(query));
+    return lakers
+      .filter((item) => item.includes(query))
+      .map((item) => ({
+        value: item,
+      }));
   };
+
   return (
     <AutoComplete
       fetchSuggestions={handleFetch}
@@ -30,4 +35,47 @@ const SimpleComplete = () => {
   );
 };
 
-storiesOf("AutoComplete Component", module).add("AutoComplete", SimpleComplete);
+interface IPlayer {
+  name: string;
+  number: number;
+}
+
+const CustomComplete = () => {
+  const lakersWithNumber = [
+    { value: "bradley", number: 1 },
+    { value: "pope", number: 2 },
+    { value: "carus", number: 3 },
+    { value: "cok", number: 4 },
+    { value: "cousin", number: 5 },
+    { value: "jsam", number: 6 },
+    { value: "ad", number: 7 },
+    { value: "gre", number: 8 },
+    { value: "howrad", number: 9 },
+  ];
+
+  const handleFetch = (query: string) => {
+    return lakersWithNumber.filter((item) => item.value.includes(query));
+  };
+
+  const renderOption = (item: DataSourceType) => {
+    const itemWithNumber = item as DataSourceType<IPlayer>;
+    return (
+      <>
+        <i>Name：{itemWithNumber.value}</i>
+        <p>Age：{itemWithNumber.number}</p>
+      </>
+    );
+  };
+
+  return (
+    <AutoComplete
+      fetchSuggestions={handleFetch}
+      onSelect={action("you selected !")}
+      renderOption={renderOption}
+    />
+  );
+};
+
+storiesOf("AutoComplete Component", module)
+  .add("AutoComplete", SimpleComplete)
+  .add("自定义Item", CustomComplete);
