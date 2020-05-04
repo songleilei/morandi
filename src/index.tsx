31604fa94ead3d030598da33fb60d6a64caa1f51
lei.song <moon.song@yeahmobi.com>
2020-05-04 20:00:40 +0800

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import { default as Button } from "./components/Button";
-import { default as Menu } from "./components/Menu";
+export { default as Button } from "./components/Button";
+export { default as Menu } from "./components/Menu";
 export { default as AutoComplete } from "./components/AutoComplete";
 export { default as Icon } from "./components/Icon";
 export { default as Input } from "./components/Input";
